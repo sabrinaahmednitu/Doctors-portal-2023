ae26ca3e1e4df342ade61f7037f80b998e83df9c
sabrina <sabrinaahmednitu@gmail.com>
2023-02-05 13:55:35 +0600

    firebase

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -8,13 +8,6 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
-
-  // apiKey: "AIzaSyAP4oWqKYSw-GE6hMhFUsTmdjrr77650ZY",
-  // authDomain: "doctors-portal-3d214.firebaseapp.com",
-  // projectId: "doctors-portal-3d214",
-  // storageBucket: "doctors-portal-3d214.appspot.com",
-  // messagingSenderId: "128481479897",
-  // appId: "1:128481479897:web:b583dd7961e6cd39c382c8",
 };
 
  // Initialize Firebase
@@ -24,21 +17,3 @@ const auth= getAuth(app);
 export default auth;
 
 
-
-// Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAP4oWqKYSw-GE6hMhFUsTmdjrr77650ZY",
-//   authDomain: "doctors-portal-3d214.firebaseapp.com",
-//   projectId: "doctors-portal-3d214",
-//   storageBucket: "doctors-portal-3d214.appspot.com",
-//   messagingSenderId: "128481479897",
-//   appId: "1:128481479897:web:b583dd7961e6cd39c382c8"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
